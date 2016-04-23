c18f145a5b2d483e6bbd1cd12ba161f955e6b7d7
marianneskolander <marianne.skolander@gmail.com>
2016-04-23 13:42:42 +0200

    Initial commit

---
+++ app/src/main/java/org/projects/shoppinglist/MainActivity.java
@@ -0,0 +1,284 @@
+package org.projects.shoppinglist;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<Product> adapter;
+    ListView listView;
+    ArrayList<Product> bag = new ArrayList<Product>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    private String[] items = { " ttt 0", "ttt 1", "ttt 2", "tt 3", "ttt 4",
+            "ttt 5", "ttt 6", "ttt 7" };
+
+    public ArrayAdapter getMyAdapter() {
+        return adapter;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    //-----------------------
+        //getting our listiew - you can check the ID in the xml to see that it
+        //is indeed specified as "list"
+
+        if (savedInstanceState != null) {
+            bag = savedInstanceState.getParcelableArrayList("savedList");
+        }
+        listView = (ListView) findViewById(R.id.list);
+        //here we create a new adapter linking the bag and the
+        adapter = new ArrayAdapter<Product>(this,
+                android.R.layout.simple_list_item_checked, bag);
+        //setting the adapter on the listview
+        listView.setAdapter(adapter);
+        //here we set the choice mode - meaning in this case we can
+        //only select one item at a time.
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        //----------------spinner------------------------------------------------------------------
+
+
+        final Spinner sp3= (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adp3=ArrayAdapter.createFromResource(this,
+                R.array.str2, android.R.layout.simple_list_item_1);
+
+        adp3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp3.setAdapter(adp3);
+        sp3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+                // TODO Auto-generated method stub
+                String ss=sp3.getSelectedItem().toString();
+                Toast.makeText(getBaseContext(),ss , Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+        });
+
+        //int position = spinner.getSelectedItemPosition();
+        //String item = (String) spinner.getSelectedItem();
+
+
+//--------------------addButton---------------------------------------------------------------------------
+        Button addButton = (Button) findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText itemTxt = (EditText) findViewById(R.id.itemInput);
+                //EditText numberTxt = (EditText) findViewById(R.id.numberInput);
+                String ss=sp3.getSelectedItem().toString();
+
+                Integer howMany = Integer.parseInt(ss.toString());
+
+                bag.add(new Product(itemTxt.getText().toString(), howMany));
+                //bag.add(new Product(itemTxt.getText().toString(), ss));
+                itemTxt.setText("");
+                //numberTxt.setText("1");//
+                getMyAdapter().notifyDataSetChanged();
+
+            }
+        });
+
+/*//--------------------Dialog til delete knap------------------------
+    public void showDialog(View v) {
+        //showing our dialog.
+        MyDialogFragment dialog = new MyDialogFragment() {
+            @Override
+            protected void positiveClick() {
+                //Here we override the methods and can now
+                //do something
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "positive button clicked", Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+            @Override
+            protected void negativeClick() {
+                //Here we override the method and can now do something
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "negative button clicked", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        };
+
+        //Here we show the dialog
+        //The tag "MyFragement" is not important for us.
+        dialog.show(getFragmentManager(), "MyFragment");
+    }*/
+
+
+//--------------------------------deletebutton--------------------------
+
+        Button deleteButton = (Button) findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selected = listView.getCheckedItemPosition();
+                bag.remove(selected);
+                getMyAdapter().notifyDataSetChanged();
+            }
+        });
+
+/*
+//---------------------------------Clearbutton---------------------------------
+
+        Button clearButton = (Button) findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bag.clear();
+                //int selected = ;
+                getMyAdapter().notifyDataSetChanged();
+            }
+        });
+*/
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+//------------------------------dialog test----------------------------------------------
+public void showDialog(View v) {
+    //showing our dialog.
+    MyDialogFragment dialog = new MyDialogFragment() {
+        @Override
+        protected void positiveClick() {
+            //Here we override the methods and can now
+            //do something
+            //Toast toast = Toast.makeText(getApplicationContext(),
+             //       "positive button clicked", Toast.LENGTH_LONG);
+            //toast.show();
+            bag.clear();
+            //int selected = ;
+            getMyAdapter().notifyDataSetChanged();
+        }
+
+        @Override
+        protected void negativeClick() {
+            //Here we override the method and can now do something
+            //oast toast = Toast.makeText(getApplicationContext(),
+                   // "negative button clicked", Toast.LENGTH_SHORT);
+            //toast.show();
+        }
+    };
+
+    //Here we show the dialog
+    //The tag "MyFragement" is not important for us.
+    dialog.show(getFragmentManager(), "MyFragment");
+}
+
+//-------------------------------------------------------------------------------------------
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*----------------tilføjet-------------------------*/
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //......................outState.putStringArrayList("savedList", bag);
+        outState.putParcelableArrayList("savedList", bag);
+
+        //outState.putInt("selected");
+        //outState.putString("savedpos", );
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.projects.shoppinglist/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://org.projects.shoppinglist/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
---
+++ app/src/main/java/org/projects/shoppinglist/MyDialogFragment.java
@@ -0,0 +1,52 @@
+package org.projects.shoppinglist;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class MyDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        //Here we create a new dialogbuilder;
+        AlertDialog.Builder alert = new AlertDialog.Builder(
+                getActivity());
+        alert.setTitle("Confirmation");
+        alert.setMessage("Are you sure?");
+        alert.setPositiveButton("Yes", pListener);
+        alert.setNegativeButton("No", nListener);
+
+        return alert.create();
+    }
+
+//--------------- yes button-----------------------------------------------------------
+    DialogInterface.OnClickListener pListener = new DialogInterface.OnClickListener() {
+
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            positiveClick();
+        }
+    };
+
+//-----------------------no button------------------------------------------------------
+    DialogInterface.OnClickListener nListener = new DialogInterface.OnClickListener() {
+
+        @Override
+        public void onClick(DialogInterface arg0, int arg1) {
+            negativeClick();
+        }
+    };
+
+    //These two methods are empty, because they will be overridden
+
+    protected void positiveClick()
+    {
+
+    }
+    protected void negativeClick()
+    {
+
+    }
+}
